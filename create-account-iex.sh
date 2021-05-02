c6fb591c7ced0c0d581e00ea8c69abb2403ec556
Péter Szakszon <szp0530@gmail.com>
2021-05-02 16:05:39 +0200

    Create iex cloud account

--- create-account-iex.sh
+++ create-account-iex.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #set -x
- 
+
 for i in `cat tickers.txt`; do
-    if grep -q "$i," tokens.csv; then
+    if grep -q "^$i," tokens.csv; then
         echo "Found: $i"
     else
         echo "Not found: $i"
@@ -12,7 +12,7 @@ for i in `cat tickers.txt`; do
 
         token="$(echo $output | sed -nr "s/.*(pk_[a-z0-9_]{32}).*/\1/p")"
         echo "TOKEN: $token"
-        sed -i "s/$i,/$i,$token/g" tokens.csv
+        sed -i "s/^$i,/$i,$token/g" tokens.csv
     fi
 done
 
